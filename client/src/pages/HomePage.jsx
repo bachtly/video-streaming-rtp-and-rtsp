a403b6c78c421ddd9233613baf09a6849ba596f6
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2021-11-08 21:09:23 +0700

    Modify UI

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,47 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import io from 'socket.io-client'
-
+import {Box} from '@mui/material'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import FastForwardIcon from '@mui/icons-material/FastForward'
+import FastRewindIcon from '@mui/icons-material/FastRewind'
+import StopIcon from '@mui/icons-material/Stop';
+import PauseIcon from '@mui/icons-material/Pause';
 
 function HomePage() {
-	var arr_b = new ArrayBuffer(16);
-	var arr = new Int8Array(arr_b);
-	arr[0]=1; arr[1]=1; arr[4]=1; arr[5]=1;
-	console.log(arr);
+	// var arr_b = new ArrayBuffer(16);
+	// var arr = new Int8Array(arr_b);
+	// arr[0]=1; arr[1]=1; arr[4]=1; arr[5]=1;
+	// console.log(arr);
 
-	var arr_s = arr.slice(0,4);
-	console.log(arr_s);	
-	console.log(arr_s.length);	
+	// var arr_s = arr.slice(0,4);
+	// console.log(arr_s);	
+	// console.log(arr_s.length);	
 
-	var socket = '';
-	// Sends a message to the server via sockets
-	const setup = () => {
-		socket = io.connect('ws://127.0.0.1:1410')
-		socket.on('connect',function() {
-			console.log('Client has connected to the server!');
-		});
+	// var socket = '';
+	// // Sends a message to the server via sockets
+	// const setup = () => {
+	// 	socket = io.connect('ws://127.0.0.1:1410')
+	// 	socket.on('connect',function() {
+	// 		console.log('Client has connected to the server!');
+	// 	});
 
-		socket.on('msg',function(data) {
-			console.log('Received a message from the server!',data);
-		});
+	// 	socket.on('msg',function(data) {
+	// 		console.log('Received a message from the server!',data);
+	// 	});
 
-		socket.on('disconnect',function() {
-			console.log('The client has disconnected!');
-		});
-		console.log('In setup');
-		socket.emit('msg','Setup send');
-	};
-	const test = () => {
-		console.log('In test');
-		socket.send('msg','Test send');
-	};
+	// 	socket.on('disconnect',function() {
+	// 		console.log('The client has disconnected!');
+	// 	});
+	// 	console.log('In setup');
+	// 	socket.emit('msg','Setup send');
+	// };
+	// const test = () => {
+	// 	console.log('In test');
+	// 	socket.send('msg','Test send');
+	// };
 
+	const [imgSrc, setImgSrc] = useState('');
+	const [isPlay, setIsPlay] = useState(false);
+
+	const handleOnClickPlaybtn = () => {
+		setIsPlay(!isPlay);
+	}
+  
 	return (
-		<div>
-				<h1>HomePage</h1>
-				
-				<button onClick={setup} style={{width: 100, height: 30}}>SETUP</button>
-				<button onClick={test} style={{width: 100, height: 30}}>RUN</button>
-		</div>
+		<Box>
+			<Box sx={{justifyContent: 'center', display:'flex'}}>HomePage</Box>
+			<Box sx={{justifyContent: 'center', display:'flex'}}>
+				<img src={imgSrc} alt='Video'/>
+			</Box>
+			<Box sx={{justifyContent: 'center', display:'flex'}}>
+				<Box><FastRewindIcon/></Box>
+				<Box id="playbtn" onClick={handleOnClickPlaybtn}>{
+					isPlay? <PauseIcon/> : <PlayArrowIcon/>
+				}</Box>
+				<Box><FastForwardIcon/></Box>
+				<Box><StopIcon/></Box>
+			</Box>
+		</Box>
 	)
 }
 
